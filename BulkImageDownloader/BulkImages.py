c3a1aac29610f805930a1cfd9f6561537ed1ddce
RobVor <31891938+RobVor@users.noreply.github.com>
2018-01-01 15:20:33 +0200

    Updated downloader
    Removed the file clearing function at the end of the script and replaced it with a dynamic removal of links already downloaded. As a file is downloaded, it is removed from the "Links.txt" file.
    
    Also included a 'Limit' to the amount of downloads. In this case 1000 links at a time. This allows the script or complied app, to be scheduled and will only fetch a certain amount, 1000, of records each run.
    
    Currently using this on a Windows machine with a scheduled task that runs a compiled exe of this script every hour after startup. This allows control of when and how much downloading is done at a time. Bulk links can then be downloaded during normal operation and does not require the system to remain on to process all of the requests, it will get there, eventually.
    
    Lastly, for any links that produced an error, an error file will be created with the links that caused the download error, if any.

--- BulkImageDownloader/BulkImages.py
+++ BulkImageDownloader/BulkImages.py
@@ -20,7 +20,7 @@ __deprecated__ = False
 __email__ =  "rob.vor@gmail.com"
 __maintainer__ = "Robert Vorster"
 __status__ = "Production"
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 import pathlib, sys, re, os, urllib.request
 cwd = os.path.dirname(os.path.abspath(__file__))
@@ -35,20 +35,39 @@ opener=urllib.request.build_opener()
 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
 
+Limit = 0
+
 for link in Links:
+    Limit += 1
     link = link.replace("\n","")
     filename = os.path.basename(link).split("?")[0]
     filename = os.path.dirname(sys.executable) + "/" + filename
     fileExists = pathlib.Path(filename)
+    if Limit == 1000:
+        sys.exit()
     try:
         if fileExists.is_file():
             print("File exists, skipping file, downloading next...")
+            with open(sourceFile, "r+") as fileUpdate:
+                OldFile = fileUpdate.readlines()
+                fileUpdate.seek(0)
+                for item in OldFile:
+                    if item != str(link)+"\n":
+                        fileUpdate.write(item)
+                fileUpdate.truncate()
+            pass
         else:
             urllib.request.urlretrieve(link,filename)
+            with open(sourceFile, "r+") as fileUpdate:
+                OldFile = fileUpdate.readlines()
+                fileUpdate.seek(0)
+                for item in OldFile:
+                    if item != str(link)+"\n":
+                        fileUpdate.write(item)
+                fileUpdate.truncate()
     except:
         print("The following links where rejected or produced an error, even though we changed the user agent to allow downloading.")
         print(link)
+        with open("Error_Links.txt", "a") as errFile:
+            errFile.write(str(link)+"\n")
 print("All links have been processed.")
-        
-open(sourceFile , "w").close()
-print("Links file has been emptied")
